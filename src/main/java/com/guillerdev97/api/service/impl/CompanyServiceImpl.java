ff1c089e10644c37e45b3e25c297e7c478b190a3
Guillermo García <guillermieres97@gmail.com>
2022-11-25 10:30:41 +0100

    create company service implementation with one method

---
+++ src/main/java/com/guillerdev97/api/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,24 @@
+package com.guillerdev97.api.service.impl;
+
+import com.guillerdev97.api.model.Company;
+import com.guillerdev97.api.repository.CompanyRepository;
+import com.guillerdev97.api.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getCompanies() {
+        return companyRepository.findAll();
+    }
+}
