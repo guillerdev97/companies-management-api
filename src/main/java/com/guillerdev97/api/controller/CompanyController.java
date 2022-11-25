25c5e2f0f4369a6a8d0be4d316d8e127fc648bc4
Guillermo García <guillermieres97@gmail.com>
2022-11-25 10:34:07 +0100

    create company controller with one get method

---
+++ src/main/java/com/guillerdev97/api/controller/CompanyController.java
@@ -0,0 +1,27 @@
+package com.guillerdev97.api.controller;
+
+import com.guillerdev97.api.model.Company;
+import com.guillerdev97.api.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CompanyController {
+    private CompanyService companyService;
+
+    @Autowired
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<List<Company>> getCompanies() {
+        return ResponseEntity.ok().body(companyService.getCompanies());
+    }
+}
